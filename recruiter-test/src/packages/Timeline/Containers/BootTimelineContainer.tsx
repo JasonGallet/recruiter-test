a3d5cd96b1ea8e1d66b1626ea0cf7e0071a2024e
Jason <jason.gallet@itsfactory.fr>
2018-11-26 17:53:30 +0100

    [BT] Added observer decorator on container

--- recruiter-test/src/packages/Timeline/Containers/BootTimelineContainer.tsx
+++ recruiter-test/src/packages/Timeline/Containers/BootTimelineContainer.tsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { BootTimelineComponent } from '../Components/BootTimelineComponent';
 import { BootTimelineStore } from '../BootTimelineStore';
+import { observer } from "mobx-react";
 
 const btStore = new BootTimelineStore();
 
+@observer
 class BootTimelineContainer extends Component {
     public render() {
         return (
